ff606feac190bb09dd45113480c170104c147661
srijal30 <srijal30@stuy.edu>
2022-08-12 00:52:36 -0400

    added create account tests

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+import os
+from typing import Any, Dict
+from uuid import uuid4
+
+import pytest
+from fastapi.testclient import TestClient
+
+from src.app import app
+
+
+@pytest.fixture(scope='session', autouse=True)
+def database_management():
+    os.system("rm data.sqlite")
+    os.system("prisma db push")
+    yield
+
+
+@pytest.fixture()
+def test_client() -> TestClient:
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope='session')
+def test_user(test_client: TestClient) -> Dict[str, Any]:
+    create_payload = \
+    {
+        'email': 'ben@dover.com',
+        'phone': '3476538334',
+        'password': 'bendover',
+        'firstName': 'Login',
+        'lastName': 'Test',
+    }
+    test_client.post("user/create", json=create_payload)
+    yield create_payload
+
+
+def gen_payload(name: str | None = None):
+    if name == None:
+        name = uuid4().hex[:5]
+    return \
+    {
+        'email': f'{name}@test.com',
+        'phone': '3476538334',
+        'password': 'test',
+        'firstName': 'Ben',
+        'lastName': 'Dover',
+    }
---
+++ tests/user/test_create.py
@@ -0,0 +1,44 @@
+from typing import Dict, Any
+
+from uuid import uuid4
+from fastapi.testclient import TestClient
+
+from tests.conftest import gen_payload
+
+def test_creation_without_optional_param(test_client: TestClient):
+    res = test_client.post("/user/create", json=gen_payload())
+    assert res.status_code == 200
+    json = res.json()
+    assert json['success']
+
+
+def test_creation_with_optional_param(test_client: TestClient):
+    req = {**gen_payload(), 'sendNotifications': False}
+    res = test_client.post("user/create", json=req)
+    assert res.status_code == 200
+    json = res.json()
+    assert json['success']
+
+
+def test_creation_without_email_param(test_client: TestClient):
+    no_email = gen_payload()
+    no_email.pop('email')
+    res = test_client.post("/user/create", json=no_email)
+    assert res.status_code == 422
+
+
+def test_creation_without_unique_email(test_client: TestClient):
+    test_client.post("/user/create", json=gen_payload('samename'))  # prerequisite
+    res = test_client.post("/user/create", json=gen_payload('samename'))
+    assert res.status_code == 200
+    json = res.json()
+    assert not json['success']
+    assert json['message'] == 'There is already a user with that email!'
+
+
+# def test_creation_invalid_email():
+#     pass
+
+
+# def test_creation_invalid_phone():
+    # pass    
