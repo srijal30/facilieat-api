81134561a4ff3d5918df7a7984370fc0d6d8e7b2
srijal30 <srijal30@stuy.edu>
2022-08-12 00:53:59 -0400

    fix: updated import for app

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 
-from .routers.user import user_router
+from .routers.user_router import user_router
+from .database import db
 
 app = FastAPI()
 app.include_router(user_router)
 
+
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()
+
+
 @app.get("/")
 async def root_page():
-    return {'working':True}
+    return {'working': True}
