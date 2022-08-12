760a67f9cfdaa0730228b8b4a904e2355cc4316e
srijal30 <srijal30@stuy.edu>
2022-08-12 00:53:32 -0400

    finished account creatoin + started login

---
+++ src/routers/user_router.py
@@ -0,0 +1,61 @@
+from typing import Any, Dict
+from fastapi import APIRouter
+
+from ..database import db
+from ..types.user import UserIn, LogIn
+from ..utils import response, create_token
+
+
+user_router = APIRouter(prefix="/user")
+
+
+@user_router.post("/create")
+async def create_account(user: UserIn) -> Dict[str, Any]:
+    """Creates a new user account through client request"""
+    # check for user with same email
+    existing_user = await db.user.find_unique(where={'email': user.email})
+    if existing_user:
+        return response(False, 'There is already a user with that email!')
+
+    # make sure new uuid is generated (maybe this can go in the model too?)
+    existing_user = await db.user.find_unique(where={'userId': user.userId})
+    while existing_user:
+        user.new_uuid()
+        existing_user = await db.user.find_unique(
+            where={'userId': user.userId}
+        )
+
+    # hash the password (maybe this can go in the model?)
+    user.hash()
+
+    # add user to db
+    await db.user.create(data=user.dict())
+
+    # return success
+    return response(True, 'User successfully created!')
+
+
+@user_router.post("/login")
+async def login_user(login: LogIn) -> Dict[str, Any]:
+    """Logs user in if information is correct"""
+    requested_user = await db.user.find_unique(where={'email': login.email})
+    # check if email exists
+    if not requested_user:
+        return response(False, 'No user with that email!')
+
+    # check if wrong password
+    login.hash()  #? maybe this can go in model
+    if requested_user.password != login.password:
+        return response(False, 'Invalid email or password!')
+
+    # create and return token
+    token = create_token(requested_user.userId)
+    return response(
+        success=True, 
+        message='Login was successful!',
+        data={
+            "token": token
+        }
+    )
+
+
---
+++ src/utils.py
@@ -0,0 +1,51 @@
+from typing import Dict, Any
+from hashlib import sha256
+
+from jwt import encode, decode
+
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+
+def hash_password(password: str) -> str:
+    """Returns hashed password"""
+    salted = password + os.getenv('SALT')
+    return sha256(salted.encode()).hexdigest()
+
+
+def response(
+    success: bool,
+    message: str,
+    data: Dict[str, Any] = {}
+) -> Dict[str, Any]:
+    """Creates a response that will be sent to the user"""
+    return \
+        {
+            'success': success,
+            'message': message,
+            'data': data
+        }
+
+
+# ADD AN EXPIRATION
+def create_token(userId: str) -> str:
+    """Creates a JWT for authenticated user"""
+    json = {
+        'userId': userId
+    }
+    token = encode(
+        payload=json,
+        key=os.getenv('JWT'),
+        algorithm='HS256'
+    )
+    return token
+
+
+def validate_token(token: str) -> str | None:
+    """Returns userId if valid token, else returns None"""
+    try:
+        return decode(token, os.getenv('JWT'), 'HS256')
+    except Exception:
+        return None
